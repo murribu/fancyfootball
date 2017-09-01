664cf649a1b7dd2c516d9755f8074e3d34478c67
Cory Martin <cory.martin@dozensoft.com>
2017-09-01 12:17:38 -0500

    bug fix and error reporting for calculating values

--- app/Models/League.php
+++ app/Models/League.php
@@ -69,15 +69,24 @@ class League extends Model {
             'k' => 0,
             'd-st' => 0,
         ];
+        $replacement_players = [
+            'qb' => [],
+            'rb' => [],
+            'wr' => [],
+            'te' => [],
+            'k' => [],
+            'd-st' => [],
+        ];
         $limit = 5;
         // DB::enableQueryLog();
         foreach($replacement_levels as $pos=>$points){
             $values = PlayerValue::join('player_attribute_values', 'player_attribute_values.player_id', '=', 'player_values.player_id')
                 ->join('player_attributes', 'player_attributes.id', '=', 'player_attribute_values.player_attribute_id')
                 ->where('player_attributes.name', '=', 'espn_rank')
+                ->where('player_values.league_id', $this->id)
                 ->whereRaw('player_values.player_id in (select player_id from player_position where position_id = (select id from positions where slug = ?))', [$pos])
                 ->whereRaw('player_values.player_id not in (select player_id from universe where active = 1 and league_id = ?)', [$this->id])
-                ->select('player_values.id', 'player_values.points')
+                ->select('player_values.id', 'player_values.player_id', 'player_values.points')
                 ->orderByRaw('cast(player_values.points as signed) desc, cast(player_attribute_values.value as signed)')
                 ->limit($limit)
                 ->get();
@@ -85,19 +94,29 @@ class League extends Model {
             // dd(DB::getQueryLog());
             foreach($values as $v){
                 $avg += $v->points;
+                // $replacement_players[$pos][] = Player::find($v->player_id);
             }
             
             $replacement_levels[$pos] = $avg/$limit;
         }
+
+        // var_dump($replacement_levels);
+        // var_dump($replacement_players);
         
+        // echo '<table><tr><td>Name</td><td>Points</td><td>Replacement</td><td>Above Replacement</td></tr>';
         foreach($players as $p){
+            if (count($p->positions) > 1) {
+                dd('Too many positions for '. $p->first_name.' '. $p->last_name);
+            };
             if (count($p->player_values($this->id)) > 0){
                 $proj = $p->player_values($this->id);
                 $proj->points_above_replacement = 
                     $proj->points - $replacement_levels[$p->positions[0]->slug];
                 $proj->save();
+                // echo '<tr><td>'. $p->first_name.' '. $p->last_name.'</td><td>'.$proj->points.'</td><td>'.$replacement_levels[$p->positions[0]->slug].'</td><td>'.$proj->points_above_replacement.'</td></tr>';
             }
         }
+        // echo '</table>';
         // dd(DB::getQueryLog());
         return $replacement_levels;
     }
